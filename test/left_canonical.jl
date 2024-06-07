f4be1bacf4f0cd52f98efe6028cd54611fbc5bf4
Huanhai Zhou <albus.zhouhh@gmail.com>
2024-06-07 16:32:25 +0800

    test: Add test cases for verifying left canonicality after updating

--- test/left_canonical.jl
+++ test/left_canonical.jl
@@ -3,6 +3,7 @@ using IsometricPEPS
 using LinearAlgebra
 using OMEinsum
 using Test
+using Zygote
 
 @testset "left canonical" begin
     D = 128;
@@ -18,6 +19,24 @@ using Test
     isometric_mps = left_canonicalize!(mps);
     @test length(isometric_mps.tensors) == n
     @test size(isometric_mps.tensors[end], 3) == 2 * χ
+
+    contracted = ein"ia, ib->ab"(isometric_mps.left, conj(isometric_mps.left));
+    @test contracted ≈ Diagonal(ones(ComplexF64, size(contracted, 1)))
+
+    for i = 1:length(isometric_mps.tensors)
+        contracted = ein"(ab, aic), bid->cd"(contracted, isometric_mps.tensors[i], conj(isometric_mps.tensors[i]));
+        @test contracted ≈ Diagonal(ones(ComplexF64, size(contracted, 1)))
+    end
+
+    for _ in 1:5
+        G = Zygote.gradient(x -> begin
+            energy = transverse_Ising_1d_energy(x, 1.0, 1.0);
+    
+            return energy
+        end, isometric_mps)[1];
+    
+        steifel_update!(isometric_mps, G, tau);
+    end
 
     contracted = ein"ia, ib->ab"(isometric_mps.left, conj(isometric_mps.left));
     @test contracted ≈ Diagonal(ones(ComplexF64, size(contracted, 1)))
