9b5cb5793c33f9199f348d6886004a3ab9e51697
Documenter.jl <documenter@juliadocs.github.io>
2024-06-07 07:30:23 +0000

    build based on 3448987

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IsometricPEPS","category":"page"},{"location":"#IsometricPEPS","page":"Home","title":"IsometricPEPS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for IsometricPEPS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [IsometricPEPS]","category":"page"}]
+}
